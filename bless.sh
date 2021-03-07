a2bb373dad047d6e3426d3e776b71a36a2801a2d
zhuyahaidong <2228703943@qq.com>
2021-03-07 23:59:00 +0800

    a shell file

---
+++ bless.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "Please input one natrual number: " a
+read -p "This time another natrual number: " b
+if [ $a -gt $b ]
+    then echo "you are a great guy"
+    else echo "sorry for that"
+fi
+
